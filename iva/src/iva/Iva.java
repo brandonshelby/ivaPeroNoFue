7509d6463755e5aadd1bb6e5e11c7d5bde770e4e
brandonshelby <145580496+brandonshelby@users.noreply.github.com>
2023-09-27 07:37:36 -0600

    ta chido

---
+++ iva/src/iva/Iva.java
@@ -0,0 +1,15 @@
+
+package iva;
+import java.util.Scanner;{
+public class Iva {
+public static void main(String[] args) {
+   
+Scanner.leer=new Scanner(System.in);
+float n1,total,iva,pluma,neto;
+  pluma =10;
+  Sisten.out.print("cuantas plumas de 10 pesos quieres");
+  
+
+}
+}
+}
